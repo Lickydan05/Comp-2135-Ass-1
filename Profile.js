8fc8afd851a60c2514af71f3c071b4c1ab8d1ca0
Lickydan05 <102684129+Lickydan05@users.noreply.github.com>
2023-02-17 01:58:58 +0000

    commit

---
+++ Profile.js
@@ -0,0 +1,44 @@
+              
+
+
+                   function submitFunction() {
+                      let postal_code = /[A-Z]\d[A-Z]\s\d[A-Z]\d/ 
+                      let x = document.getElementById("frm1");
+                      var text = "";
+                      let i = 2;
+                      text += x.elements[i].value;
+
+                      if (text.match(postal_code)) {
+                      document.getElementById("demo").innerHTML = "Your Postal Code is " + text + ", correct?";
+                     } else if (!text.match(postal_code)){
+                      document.getElementById("demo").innerHTML = "Please enter a valid Postal Code 'X0X 0X0'";
+                     };
+                    };
+
+                  function resetForm() {
+                      document.getElementById("frm1").reset();
+                    };
+
+                  function time() {
+                    new Date().toLocaleTimeString();
+                    setTimeout("time()",1000);
+                    document.getElementById("Date").innerHTML = Date();
+                  }
+
+
+
+             /*       var User = "User"
+
+                    var Usertest = prompt("Enter your Username")
+                    if (User != Usertest) {
+                      alert("Wrong Username, Try again");
+                      location.reload(true);
+                    }
+
+                    var Password = "Pass"
+
+                    var Passtest = prompt("Enter your Password")
+                    if (Password != Passtest) {
+                      alert("Wrong Password, Try again");
+                      location.reload(true);
+                    } */
