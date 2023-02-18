f08fa4dec419ebf747d9bd42d5fc804dc4efdfc5
Lickydan05 <102684129+Lickydan05@users.noreply.github.com>
2023-02-18 03:08:14 +0000

    kermitted

--- Dashboard.js
+++ Dashboard.js
@@ -4,28 +4,29 @@
                       output.innerHTML = (slider.value/2 + "°C"); 
 
                       slider.oninput = function() { 
-                      output.innerHTML = this.value/2 + "°C";
+                      output.innerHTML = this.value/2 + "°C"; // Temp slider values
                     };
 
-                   function submitFunction() {
+                   function submitFunction() { 
                       let postal_code = /[A-Z]\d[A-Z]\s\d[A-Z]\d/ 
                       let x = document.getElementById("frm1");
                       var text = "";
                       let i = 2;
                       text += x.elements[i].value;
 
-                      if (text.match(postal_code)) {
+                      if (text.match(postal_code)) { //checks postal code
                       document.getElementById("demo").innerHTML = "Your Postal Code is " + text + ", correct?";
                      } else if (!text.match(postal_code)){
                       document.getElementById("demo").innerHTML = "Please enter a valid Postal Code 'X0X 0X0'";
                      };
                     };
 
-                  function resetForm() {
+                  function resetForm() { //resets form
                       document.getElementById("frm1").reset();
+                      output.innerHTML = (slider.value/2 + "°C");
                     };
 
-                  function time() {
+                  function time() { //time + date
                     new Date().toLocaleTimeString();
                     setTimeout("time()",1000);
                     document.getElementById("Date").innerHTML = Date();
--- Profile.js
+++ Profile.js
@@ -1,44 +1,43 @@
-              
-
-
-                   function submitFunction() {
-                      let postal_code = /[A-Z]\d[A-Z]\s\d[A-Z]\d/ 
-                      let x = document.getElementById("frm1");
-                      var text = "";
-                      let i = 2;
-                      text += x.elements[i].value;
-
-                      if (text.match(postal_code)) {
-                      document.getElementById("demo").innerHTML = "Your Postal Code is " + text + ", correct?";
-                     } else if (!text.match(postal_code)){
-                      document.getElementById("demo").innerHTML = "Please enter a valid Postal Code 'X0X 0X0'";
-                     };
-                    };
-
-                  function resetForm() {
+                  function resetForm() { //resets form
                       document.getElementById("frm1").reset();
                     };
 
-                  function time() {
+                  function time() { //setting time
                     new Date().toLocaleTimeString();
                     setTimeout("time()",1000);
                     document.getElementById("Date").innerHTML = Date();
                   }
 
-
-
-             /*       var User = "User"
-
-                    var Usertest = prompt("Enter your Username")
-                    if (User != Usertest) {
-                      alert("Wrong Username, Try again");
-                      location.reload(true);
+                  function submitFunction() { //getting different form values
+                    let email_check = /[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]+/;
+                    let x = document.getElementById("frm1");
+                    var text = "";
+                    let i = 2;
+                    text += x.elements[i].value;
+                    var passlength = "";
+                    let b = 4;
+                    passlength += x.elements[b].value;
+                    let c = 5 
+                    var confirm_pass = "";
+                    confirm_pass += x.elements[c].value;
+                    var passleng = /(.*[a-z].*)(.*[A-Z].*)(.*\d.*)/
+                    console.log(confirm_pass);
+                    
+                    if (passlength.length >= 8 && passlength.match(passleng) && passlength == confirm_pass) { //checks that the passwords meet the requirements
+                    } else {
+                      document.getElementById("demo2").innerHTML = "Your password needs to be minimum 8 characters with 1 lower and 1 upper-case character.";
                     }
 
-                    var Password = "Pass"
+                    if (text.match(email_check)) { //Checks the email
+                      document.getElementById("demo1").innerHTML = "Your Email is " + text + ", correct?";
+                     } else if (!text.match(email_check)){
+                      document.getElementById("demo1").innerHTML = "Please enter a valid Email Address 'XXX@XXXX.XXX'";
+                     };
 
-                    var Passtest = prompt("Enter your Password")
-                    if (Password != Passtest) {
-                      alert("Wrong Password, Try again");
-                      location.reload(true);
-                    } */
+                      var elements = document.getElementById("frm1").elements; //Make the whitespaces unable to be submitted
+                      for(var a = 0 ; a < elements.length ; a++){
+                          
+                      }
+                  
+                      document.getElementById("demo3").innerHTML = item
+                    };
